eafc6b0bd6845eef0b9eac20896dd030930efd3f
Dhaval Patel <dhaval.patel@kernelequity.com>
2024-04-23 12:14:54 +0530

    Initial Commit

---
+++ ios-github-action-demo/ios_github_action_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_github_action_demoApp.swift
+//  ios-github-action-demo
+//
+//  Created by Dhaval Patel on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_github_action_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
